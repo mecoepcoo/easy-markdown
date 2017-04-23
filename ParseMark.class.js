224fc6bbb7bdf491499fce977b7c05883a3e3ded
tianzhen <340349237@qq.com>
2017-04-23 20:18:32 +0800

    init

---
+++ ParseMark.class.js
@@ -0,0 +1,20 @@
+/**
+ * Created by tianzhen on 17-4-23.
+ */
+var ParseMark = (function () {
+    function ParseMark() {
+    }
+    /*获取文本，文本按html格式解析*/
+    ParseMark.prototype.getHtml = function () {
+    };
+    /*获取文本，文本按字符串解析，特殊符号将被转义*/
+    ParseMark.prototype.getText = function () {
+    };
+    /**
+     * 获取一行
+     * @param lineNum 行号
+     */
+    ParseMark.prototype.getLine = function (lineNum) {
+    };
+    return ParseMark;
+}());
---
+++ ParseMark.class.ts
@@ -0,0 +1,25 @@
+/**
+ * Created by tianzhen on 17-4-23.
+ */
+class ParseMark {
+    /*获取文本，文本按html格式解析*/
+    public getHtml() {
+
+    }
+
+    /*获取文本，文本按字符串解析，特殊符号将被转义*/
+    public getText() {
+
+    }
+
+    /**
+     * 获取一行
+     * @param lineNum 行号
+     */
+    public getLine(lineNum: number) {
+
+    }
+
+
+}
+
